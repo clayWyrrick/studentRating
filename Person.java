e1683ee882ca7d03b5ba5b93e7e601d4c041ac2c
Clay Wyrrick <clay.wyrrick@gmail.com>
2019-10-14 13:44:50 -0500

    add Person.java

---
+++ Person.java
@@ -0,0 +1,28 @@
+
+public class Person extends NameHolder {
+	private String lastName;
+	
+	public Person() throws NameException
+	{
+		String temp1 = "Bo";
+		String temp2 = "Smith";
+		lastName = temp1;
+		super.setFirstName(temp2);
+	}
+	
+	public Person (String firstName, String lastName) throws NameException
+	{
+		super.setFirstName(firstName);
+		
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String describeSelf() {
+		String description = "First Name: " + super.getFirstName()
+				+ "Last name: " + lastName;
+		return description;
+
+	}
+
+}
