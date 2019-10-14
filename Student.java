fa50877b1f3bb35346884821af3ead4a1501ec4f
Clay Wyrrick <clay.wyrrick@gmail.com>
2019-10-14 14:03:40 -0500

    Sketched a new class called Student (with rating)

---
+++ Student.java
@@ -0,0 +1,15 @@
+
+public class Student implements Comparable<Student> {
+	private String className;
+	private Integer rating;
+	
+	
+	@Override
+	public int compareTo(Student arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
