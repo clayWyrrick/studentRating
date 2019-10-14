463981ab18deed7310e7fcddda45548fceeadd3c
Clay Wyrrick <clay.wyrrick@gmail.com>
2019-10-14 13:43:41 -0500

    NameHolder class

---
+++ NameHolder.java
@@ -0,0 +1,27 @@
+
+public abstract class NameHolder {
+	protected String firstName;
+	
+	public abstract String describeSelf();
+	
+	@Override
+	public String toString()
+	{
+		return "First name: " + firstName;
+	}
+	
+	protected void setFirstName(String firstName) throws NameException
+	{
+		if(!firstName.isEmpty())
+			this.firstName = firstName;
+		else
+			throw new NameException("Wrong");
+				
+	}
+	
+	protected String getFirstName()
+	{
+		return firstName;
+	}
+
+}
